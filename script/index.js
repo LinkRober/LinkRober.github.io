a1c587a993cccfa608073ea869ddb4d97d34f868
Robert <min.xia@quvideo.com>
2017-02-15 11:27:52 +0800

    merge

--- script/index.js
+++ script/index.js
@@ -1,4 +1,13 @@
 function moveMenu(){
     var leftDocument = document.getElementById("left_js");
     leftDocument.style.width = "0px";
+    // var nameDocument = document.getElementById("left_js.span");
+    
+    // var userHeadDocument = document.getElementById("userhead_js");
+    // userHeadDocument.style.width = "0px";
+
+    // var nameDocument = document.getElementById("name_js > span");
+    // nameDocument.innerText = "#ff0000"
+    
+
 }
